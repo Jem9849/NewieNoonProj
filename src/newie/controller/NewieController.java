b4a9e2fbd597e190b316cb53515312706622a273
Jeremy Saysanasy <jeremysaysanasy@gmail.com>
2017-09-07 12:43:56 -0600

    Added more lines

--- src/newie/controller/NewieController.java
+++ src/newie/controller/NewieController.java
@@ -6,5 +6,13 @@ public class NewieController
 	{
 		System.out.println("TACO TUESDAY!");
 		System.out.println("IT IS VERY TAcOey.");
+		System.out.println("TACO TUESDAY!");
+		System.out.println("RAWR");
+		System.out.println("TUACAN");
+		System.out.println("BIRDS");
+		System.out.println("Squirells");
+		System.out.println("RACOONS");
+		System.out.println("TRASH HAT");
+		System.out.println("NIAGRA FALLS");
 	}
 }
