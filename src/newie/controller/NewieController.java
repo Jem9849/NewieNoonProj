ed773ef8508ab3975d8bfb476229b19583652709
Jeremy Saysanasy <jeremysaysanasy@gmail.com>
2017-10-25 12:28:19 -0600

    Added for loops and printing of arrays.

--- src/newie/controller/NewieController.java
+++ src/newie/controller/NewieController.java
@@ -1,11 +1,27 @@
 package newie.controller;
 
+import java.util.List;
+import java.util.ArrayList;
+
+
 public class NewieController
 {
+	//private ArrayList<String> myRats;
+	private String[] rats = { "", "", "", "Austrailian Swamp Rat", "Bulldog Rat"};
+	//private ArrayList<Integer> myCount;
+	private int[] count;
+	
+	public NewieController()
+	{
+		//rats = new String [5];
+		count = new int [10];
+	}
+	
 	public void start()
 	{
-		System.out.println("TACO TUESDAY!");
-		System.out.println("IT IS VERY TAcOey.");
+		/*
+		System.out.println(myRats);
+		System.out.println(myCount);
 		System.out.println("TACO TUESDAY!");
 		System.out.println("RAWR");
 		System.out.println("TUACAN");
@@ -14,5 +30,32 @@ public class NewieController
 		System.out.println("RACOONS");
 		System.out.println("TRASH HAT");
 		System.out.println("NIAGRA FALLS");
+		*/
+		
+		rats[0] = "Capybara";
+		rats[1] = "Guinea Pig";
+		rats[2] = "Ricefield Rat";
+		
+		
+		for (String current : rats)
+		{
+			System.out.println(current);
+		}
+		
+		for (int index = 0; index < count.length; index+= 1)
+		{
+			count[index] = index + 1;
+			
+			System.out.println(count[index]);
+		}
+		
+		//rats = new String [] {"one", "two", "three", "four", "five"};
+		}
+		
+		//myRats = new ArrayList<String>();
+		
+		//myCount = new ArrayList<Integer>();
+		
+		
 	}
 }
